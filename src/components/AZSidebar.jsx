29608b048834a7268a3801fb5b0909ffaef11156
Victor_YT <liyutong0918@outlook.com>
2025-06-09 02:23:07 +1000

    Update AZSidebar.jsx

--- src/components/AZSidebar.jsx
+++ src/components/AZSidebar.jsx
@@ -21,8 +21,21 @@ export default function AZSidebar({ letters }) {
                 ))}
             </div>
 
-            {/* large screen */}
+            {/* mid screen */}
             <div className="fixed top-[55%] -translate-y-1/2 left-[calc(33.333%-20px)] flex-col items-center space-y-1 z-10 hidden md:flex">
+                {letters.map(letter => (
+                    <button
+                        key={letter}
+                        onClick={() => handleScroll(letter)}
+                        className="text-gray-400 hover:text-[#AD2F4F] text-xs font-semibold"
+                    >
+                        {letter}
+                    </button>
+                ))}
+            </div>
+
+            {/* large screen */}
+            <div className="fixed top-[55%] -translate-y-1/2 left-[calc(33.333%-25px)] flex-col items-center space-y-1 z-10 hidden lg:flex">
                 {letters.map(letter => (
                     <button
                         key={letter}
