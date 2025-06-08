d7c438a0ea03b1f55ba5640da379052d202968c2
Victor_YT <liyutong0918@outlook.com>
2025-06-09 01:35:26 +1000

    update sidebar

--- src/components/AZSidebar.jsx
+++ src/components/AZSidebar.jsx
@@ -7,17 +7,34 @@ export default function AZSidebar({ letters }) {
     };
 
     return (
-        <div className="fixed md:absolute top-[55%]  -translate-y-1/2 right-2 md:right-0 md:pr-2 flex flex-col items-center space-y-1.5 z-10">
-            {letters.map(letter => (
-                <button
-                    key={letter}
-                    onClick={() => handleScroll(letter)}
-                    className="text-gray-400 hover:text-[#AD2F4F] text-xs font-semibold"
-                >
-                    {letter}
-                </button>
-            ))}
+        <div>
+            {/* small screen */}
+            <div className="fixed top-[55%] -translate-y-1/2 right-2 flex flex-col items-center space-y-1.5 z-10 md:hidden">
+                {letters.map(letter => (
+                    <button
+                        key={letter}
+                        onClick={() => handleScroll(letter)}
+                        className="text-gray-400 hover:text-[#AD2F4F] text-xs font-semibold"
+                    >
+                        {letter}
+                    </button>
+                ))}
+            </div>
+
+            {/* large screen */}
+            <div className="fixed top-[55%] -translate-y-1/2 left-[calc(33.333%-20px)] flex-col items-center space-y-1.5 z-10 hidden md:flex">
+                {letters.map(letter => (
+                    <button
+                        key={letter}
+                        onClick={() => handleScroll(letter)}
+                        className="text-gray-400 hover:text-[#AD2F4F] text-xs font-semibold"
+                    >
+                        {letter}
+                    </button>
+                ))}
+            </div>
         </div>
+
 
     );
 }
